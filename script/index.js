7a4e2d36a086a5248db224cc37af6e2a18bf2333
MaskuraBegum <maskura.begum77@gmail.com>
2024-02-21 23:51:28 +0600

    added all functionality

--- script/index.js
+++ script/index.js
@@ -3,64 +3,87 @@ const allBtn = document.getElementsByClassName('click-btn');
 let count = 0;
 let mcount = 40;
 let prize = 0;
-for(const btn of allBtn){
-    btn.addEventListener('click', function(e){
-        count = count +1;
-        btn.classList.toggle('selected')
-        if(count < 5){
-            btn.classList.add('bg-[#1DD100]')
-            mcount = mcount -1;
-        prize = prize + 500;
+for (const btn of allBtn) {
+    btn.addEventListener('click', function (e) {
 
-        document.getElementById('selected-seat').innerText = count;
-        document.getElementById('availabel-seat').innerText = mcount;
-        
-        const li = document.createElement('li')
-        const p = document.createElement('p');
-        p.innerText = btn.innerText;
-        const p2 = document.createElement('p');
-        p2.innerText = 'Economy';
-        const p3 = document.createElement('p');
-        p3.innerText = document.getElementById('taka').innerText;
+        if (!btn.classList.contains('selected')) {
+            btn.classList.add('selected')
+            count = count + 1;
 
-        li.appendChild(p);
-        li.appendChild(p2);
-        li.appendChild(p3);
-        li.classList.add('flex');
-        li.classList.add('justify-between');
-        p.classList.add('px-4');
+            if (count < 5) {
+                const li = document.createElement('li')
+                const p = document.createElement('p');
+                p.innerText = btn.innerText;
+                const p2 = document.createElement('p');
+                p2.innerText = 'Economy';
+                const p3 = document.createElement('p');
+                p3.innerText = document.getElementById('taka').innerText;
 
-        document.getElementById('list-seat').appendChild(li);
-        document.getElementById('total-taka').innerText = prize;
-        
-        if(count == 4){
-            document.getElementById('apply-btn').removeAttribute('disabled');
+                li.appendChild(p);
+                li.appendChild(p2);
+                li.appendChild(p3);
+                li.classList.add('flex');
+                li.classList.add('justify-between');
+                p.classList.add('px-4');
+
+                document.getElementById('list-seat').appendChild(li);
+
+                btn.classList.add('bg-[#1DD100]')
+                mcount = mcount - 1;
+                prize = prize + 550;
+
+                document.getElementById('selected-seat').innerText = count;
+                document.getElementById('availabel-seat').innerText = mcount;
+
+
+                document.getElementById('total-taka').innerText = prize;
+                document.getElementById('grand-prize').innerText = prize;
+            }
+            else{
+                alert('You can only select 4 seats');
+            }
         }
-        document.getElementById('grand-prize').innerText = prize;
+        else{
+            alert('You already selected this seat');
         }
-        
-        
-        
+
+        if (count < 5) {
+            if (count == 4) {
+                document.getElementById('apply-btn').removeAttribute('disabled');
+            }
+        }
     });
-    function grandTotal(){
+    function grandTotal() {
         const coupn = document.getElementById('code');
-    if(coupn.value == 'NEW15'){
-        const discount = prize - (prize*(15/100));
-        document.getElementById('grand-prize').innerText = discount;
-        document.getElementById('coupn-code').classList.add('hidden')
-    } 
-    if( coupn == "Couple 20"){
-        const discount = prize - (prize*(20/100));
-        document.getElementById('grand-prize').innerText = discount;
-    }
+        if (coupn.value == 'NEW15') {
+            const discount = prize - (prize * (15 / 100));
+            const newp =document.createElement('p');
+            newp.innerText = "Congratulation! you got discount.";
+            document.getElementById('congo').appendChild(newp);
+            newp.classList.add('w-38');
+            newp.classList.add("text-center");
+            document.getElementById('grand-prize').innerText = discount;
+            document.getElementById('coupn-code').classList.add('hidden');
+        }
+        else if (coupn.value == "Couple 20") {
+            const discount = prize - (prize * (20 / 100));
+            const newp =document.createElement('p');
+            newp.innerText = "Congratulation! you got discount";
+            document.getElementById('congo').appendChild(newp);
+            newp.classList.add('w-38');
+            newp.classList.add("text-center");
+            document.getElementById('grand-prize').innerText = discount;
+            document.getElementById('coupn-code').classList.add('hidden');
+        }
+        else{
+            alert('worng coupn code');
+        }
     }
     const phnNumb = document.getElementById('phn-number');
-    phnNumb.addEventListener('keyup',function(){
-        if(count > 0){
+    phnNumb.addEventListener('keyup', function () {
+        if (count > 0 ) {
             document.getElementById('next').removeAttribute('disabled');
         }
     });
     console.log(phnNumb);
-    
-
 }
